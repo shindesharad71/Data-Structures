6f5f0d9c9e3452fdc83ce40e802bd66b44df9c68
Sharad Shinde <shindesharad71@gmail.com>
2021-05-28 19:14:45 +0530

    Fixed: Weired error

--- 01.Array/max_sum2.py
+++ 01.Array/max_sum2.py
@@ -1,25 +1,32 @@
-# Given an array arr[] of n integers, find the maximum that maximizes 
+# Given an array arr[] of n integers, find the maximum that maximizes
 # the sum of the value of i*arr[i] where i varies from 0 to n-1.
 # https://www.geeksforgeeks.org/maximum-sum-iarri-among-rotations-given-array/
-
 def max_sum(arr: list) -> int:
-    max = 0
+    currunt_sum = 0
+    currunt_val = 0
     n = len(arr)
-    for i in range(n):
-        currunt_sum = 0
-        for j in range(len(arr)):
-            currunt_sum = currunt_sum + (j*arr[j])
 
-        if(currunt_sum > max):
-            max = currunt_sum
-        rotate_by_one(arr, n)
-    return max
+    # Calculate sum of all elements
+    for i in range(0, n):
+        currunt_sum += arr[i]
+        currunt_val += (i * arr[i])
 
-def rotate_by_one(arr: list, n: int):
-    temp = arr[0]
-    for i in range(n - 1):
-        arr[i] = arr[i + 1]
-    arr[n - 1] = temp
+    # Initialize result
+    res = currunt_val
+
+    # Compate value for other iterations
+    for i in range(1, n - 1):
+        # Compute next value using previous
+        # value in O(1) time
+        next_val = currunt_val - (currunt_sum - arr[i - 1]) + arr[i - 1] * (n - 1)
+
+        # Update current value
+        currunt_val = next_val
+
+        # Update result if required
+        res = max(res, next_val)
+
+    return res
 
 if __name__ == "__main__":
     arr = [8, 3, 1, 2]
