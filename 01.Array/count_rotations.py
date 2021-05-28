6a89fad14ca1e77030e8a7a12d6442a092b2cb35
Sharad Shinde <shindesharad71@gmail.com>
2021-05-28 19:51:33 +0530

    Added: Min Rotations Required

--- 01.Array/count_rotations.py
+++ 01.Array/count_rotations.py
@@ -2,23 +2,27 @@
 # of rotations in a sorted and
 # rotated array.
 # https://www.geeksforgeeks.org/find-rotation-count-rotated-sorted-array/
-# 
+#
 # Returns count of rotations for
 # an array which is first sorted
 # in ascending order, then rotated
 def count_rotations(arr: list, n: int) -> int:
     rotation = 0
 
-    for i in range(n):
-        rotation += 1
-        if(arr[i] > arr[i+1]):
-            break
+    # We basically find index of minimum element
+    min = arr[0]
+    for i in range(0, n):
 
-    return rotation
+        if min > arr[i]:
+
+            min = arr[i]
+            min_index = i
+
+    return min_index
 
 
 # Driver code
 if __name__ == "__main__":
-    arr: list = [7, 9, 11, 12, 15]
+    arr: list = [15, 18, 2, 3, 6, 12]
     n = len(arr)
     print(f"Rotations required: {count_rotations(arr, n)}")
