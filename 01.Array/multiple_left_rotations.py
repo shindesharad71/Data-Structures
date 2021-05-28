c9877d80f657dcdbb403668d9f0fcb3e8cf32153
Sharad Shinde <shindesharad71@gmail.com>
2021-05-28 20:24:48 +0530

    Added: Multiple Number of Rotations

---
+++ 01.Array/multiple_left_rotations.py
@@ -0,0 +1,29 @@
+# Python3 implementation of
+# left rotation of an array
+# K number of times
+# https://www.geeksforgeeks.org/quickly-find-multiple-left-rotations-of-an-array/
+
+# Function to left rotate
+# an array k times
+def leftRotate(arr, n, k):
+	
+	# Print array
+	# after k rotations
+	for i in range(k, k + n):
+		print(str(arr[i % n]),
+				end = " ")
+
+# Driver Code
+arr = [1, 3, 5, 7, 9]
+n = len(arr)
+k = 2;
+leftRotate(arr, n, k)
+print()
+
+k = 3;
+leftRotate(arr, n, k)
+print()
+
+k = 4
+leftRotate(arr, n, k)
+print()
