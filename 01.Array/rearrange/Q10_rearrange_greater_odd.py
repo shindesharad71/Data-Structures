026cf7229118c579e7dcac593d64a7e056b18b7c
Sharad Shinde <shindesharad71@gmail.com>
2021-05-30 15:18:29 +0530

    Added: More Rearrangement Problems

--- 01.Array/rearrange/Q10_rearrange_greater_odd.py
+++ 01.Array/rearrange/Q10_rearrange_greater_odd.py
@@ -4,33 +4,25 @@
 # than odd positioned elements
 # https://www.geeksforgeeks.org/rearrange-array-such-that-even-positioned-are-greater-than-odd/
 
-def assign(a, n):
-	
-	# Sort the array
-	a.sort()
-	
-	ans = [0] * n
-	p = 0
-	q = n - 1
-	for i in range(n):
-		
-		# Assign even indexes with
-		# maximum elements
-		if (i + 1) % 2 == 0:
-			ans[i] = a[q]
-			q = q - 1
-		
-		# Assign odd indexes with
-		# remaining elements
-		else:
-			ans[i] = a[p]
-			p = p + 1
-			
-	# Print result
-	for i in range(n):
-		print(ans[i], end = " ")
+def rearrange(arr, n):
 
-# Driver Code
-A = [ 1, 3, 2, 2, 5 ]
-n = len(A)
-assign(A, n)
+    for i in range(1, n):
+
+        # if index is even
+        if i % 2 == 0:
+            if arr[i] > arr[i - 1]:
+                arr[i - 1], arr[i] = arr[i], arr[i - 1]
+
+        # if index is odd
+        else:
+            if arr[i] < arr[i - 1]:
+                arr[i - 1], arr[i] = arr[i], arr[i - 1]
+
+
+if __name__ == "__main__":
+    n = 5
+    arr = [1, 3, 2, 2, 5]
+    rearrange(arr, n)
+    for i in range(n):
+        print(arr[i], end=" ")
+    print()
