acb87e3c61ee069b836cef47acb5f0d38a51aaf1
Sharad Shinde <shindesharad71@gmail.com>
2021-05-31 10:07:45 +0530

    Added: Array Implementation

---
+++ 01.Array/array_implementation.py
@@ -0,0 +1,17 @@
+# Implementing array data structure
+class DynamicArray:
+    def __init__(self):
+        super().__init__()
+
+    def append(self, element):
+        pass
+
+    def len(self):
+        pass
+
+    def remove(self, index):
+        pass
+
+    def copy(self):
+        pass
+
