f8ba5dcebe3591262e7eca299929e3589de5833a
Sharad Shinde <shindesharad71@gmail.com>
2021-06-02 10:31:28 +0530

    Added: Code Formatter

--- 02.LinkedList/linked_list.py
+++ 02.LinkedList/linked_list.py
@@ -2,5 +2,12 @@
 # Node class
 class Node:
     def __init__(self, data):
-        self.data = data # Assign Data
-        self.next = None # Initialize next as null
+        self.data = data  # Assign Data
+        self.next = None  # Initialize next as null
+
+
+# LinkedList class
+class LinkedList:
+    # Function to initialize the Linked List object
+    def __init__(self):
+        self.head = None
