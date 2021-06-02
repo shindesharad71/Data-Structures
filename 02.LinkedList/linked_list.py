f29863080b9c118dc909e08c62155444d728473d
Sharad Shinde <shindesharad71@gmail.com>
2021-06-02 10:24:28 +0530

    Added: Linked List

---
+++ 02.LinkedList/linked_list.py
@@ -0,0 +1,6 @@
+# Linked list practice and implementation
+# Node class
+class Node:
+    def __init__(self, data):
+        self.data = data # Assign Data
+        self.next = None # Initialize next as null
--- 02.Linked_List/linked_list.py
+++
@@ -1,1 +0,0 @@
-# Linked list practice and implementation
