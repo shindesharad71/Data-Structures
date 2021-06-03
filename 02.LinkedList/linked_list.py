62d689680bfe7e19462181610b2ecbfea372cdae
Sharad Shinde <shindesharad71@gmail.com>
2021-06-03 12:17:23 +0530

    Added: Get Length of LinkedList

--- 02.LinkedList/linked_list.py
+++ 02.LinkedList/linked_list.py
@@ -133,6 +133,18 @@ class LinkedList:
         # self.head = None
         # would also delete the link list
 
+    # This function counts number of nodes in Linked List
+    # iteratively, given 'node' as starting node.
+    def length(self):
+        temp = self.head  # Initialise temp
+        count = 0  # Initialise count
+
+        # Loop while end of linked list is not reached
+        while temp:
+            count += 1
+            temp = temp.next
+        return count
+
 
 # Driver Program
 if __name__ == "__main__":
