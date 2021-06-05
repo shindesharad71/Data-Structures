66560e3ff258dddb3e479cdc2ce5a87c4e0ae4c0
Sharad Shinde <shindesharad71@gmail.com>
2021-06-05 16:21:20 +0530

    Added: Remove duplicates

---
+++ 02.LinkedList/15_remove_duplicates_from_sorted_list.py
@@ -0,0 +1,14 @@
+# Remove duplicates from a sorted linked list
+# https://www.geeksforgeeks.org/remove-duplicates-from-a-sorted-linked-list/
+
+
+def remove_duplicates(self):
+    temp = self.head
+
+    while temp and temp.next:
+        if temp.data == temp.next.data:
+            new = temp.next.next
+            temp.next = None
+            temp.next = new
+        else:
+            temp = temp.next
