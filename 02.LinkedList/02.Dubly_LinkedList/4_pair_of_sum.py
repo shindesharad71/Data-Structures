5014258e671294c28696682747776d89f11bd7c5
Sharad Shinde <shindesharad71@gmail.com>
2021-06-08 19:15:48 +0530

    Added: Pair of Sum

---
+++ 02.LinkedList/02.Dubly_LinkedList/4_pair_of_sum.py
@@ -0,0 +1,34 @@
+# Find pairs with given sum in doubly linked list
+# https://www.geeksforgeeks.org/find-pairs-given-sum-doubly-linked-list/
+
+
+def pair_sum(head, x):
+    # set two pointers for start and end of the linked list
+    first = head
+    second = head
+
+    while second.next:
+        second = second.next
+
+    # To tack if we found a pair or not
+    found = False
+
+    # The loop terminates when they
+    # cross each other (second.next ==
+    # first), or they become same
+    # (first == second)
+    while first != second or second.next != first:
+        # If pair found
+        if (first.data + second.data) == x:
+            found = True
+            print("(", first.data, ",", second.data, ")")
+
+            # Move first pointer in forward direction
+            first = first.next
+
+            # Move second pointer in backward direction
+            second = second.prev
+        else:
+            second = second.prev
+
+    return found
