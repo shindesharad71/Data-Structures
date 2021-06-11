660b8041945b622e448575ac643803c766fd78c2
Sharad Shinde <shindesharad71@gmail.com>
2021-06-11 19:18:37 +0530

    Added: Stack by List

--- 03.Stack/1_stack_with_list.py
+++ 03.Stack/1_stack_with_list.py
@@ -6,3 +6,20 @@
 # top() – Returns a reference to the top most element of the stack – Time Complexity : O(1)
 # push(g) – Adds the element ‘g’ at the top of the stack – Time Complexity : O(1)
 # pop() – Deletes the top most element of the stack – Time Complexity : O(1)
+
+stack = []
+
+# append() function to push element in stack
+stack.append("A")
+stack.append("B")
+stack.append("C")
+stack.append("D")
+
+print(f"Initial Stack - {stack}")
+
+# pop() function for pop operation on stack
+print(f"Element Popped from Stack -")
+print(stack.pop())
+print(stack.pop())
+
+print(f"Stack after elements are Popped: {stack}")
