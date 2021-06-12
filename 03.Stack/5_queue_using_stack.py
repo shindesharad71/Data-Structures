560f077dbb5077c9724451c69740fb81e8fa49e0
Sharad Shinde <shindesharad71@gmail.com>
2021-06-12 15:41:45 +0530

    Added: Queue using Stack

--- 03.Stack/5_queue_using_stack.py
+++ 03.Stack/5_queue_using_stack.py
@@ -65,6 +65,43 @@ class Queue:
             return self.s2.pop()
 
 
+# ------------------------------- METHOD 3 ------------------------------
+
+# Python3 program to implement Queue using
+# one stack and recursive call stack.
+class Queue:
+    def __init__(self):
+        self.s = []
+
+    # Enqueue an item to the queue
+    def enQueue(self, data):
+        self.s.append(data)
+
+    # Dequeue an item from the queue
+    def deQueue(self):
+        # Return if queue is empty
+        if len(self.s) == 0:
+            print("Queue is empty")
+            return
+
+        # pop an item from the stack
+        x = self.s[-1]
+        self.s.pop()
+
+        # if stack become empty return the popped item
+        if len(self.s) <= 0:
+            return x
+
+        # recursive call
+        item = self.deQueue()
+
+        # push popped item back to the stack
+        self.s.append(x)
+
+        # return the result of deQueue() call
+        return item
+
+
 # Driver code
 if __name__ == "__main__":
     q = Queue()
