433fb05aee4b3f56bcce37bba915a533c1f28941
Sharad Shinde <shindesharad71@gmail.com>
2021-06-26 16:23:58 +0530

    Added: Delete Element

---
+++ 05.BinaryTree/3_delete_node.py
@@ -0,0 +1,100 @@
+# Deletion in a Binary Tree
+# https://www.geeksforgeeks.org/deletion-binary-tree/
+
+
+class Node:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.value = key
+
+
+def inorder(temp: Node):
+    if not temp:
+        return
+
+    inorder(temp.left)
+    print(temp.value, end=" ")
+    inorder(temp.right)
+
+
+# function to delete the given deepest node (d_node) in binary tree
+def delete_deepest(root, d_node):
+    q = []
+    q.append(root)
+
+    while len(q):
+        temp: Node = q.pop(0)
+
+        if temp is d_node:
+            temp = None
+            return
+
+        if temp.right:
+            if temp.right is d_node:
+                temp.right = None
+                return
+            else:
+                q.append(temp.right)
+
+        if temp.left:
+            if temp.left is d_node:
+                temp.left = None
+                return
+            else:
+                q.append(temp.left)
+
+
+# function to delete element in binary tree
+def deletion(root: Node, key):
+    if root == None:
+        return None
+
+    if root.left is None and root.right is None:
+        if root.key == key:
+            return None
+        else:
+            return root
+
+    key_node = None
+
+    q = []
+    q.append(root)
+
+    while len(q):
+        temp: Node = q.pop(0)
+
+        if temp.value == key:
+            key_node = temp
+
+        if temp.left:
+            q.append(temp.left)
+
+        if temp.right:
+            q.append(temp.right)
+
+    if key_node:
+        x = temp.value
+        delete_deepest(root, temp)
+        key_node.value = x
+
+    return root
+
+
+# Driver code
+if __name__ == "__main__":
+    root = Node(10)
+    root.left = Node(11)
+    root.left.left = Node(7)
+    root.left.right = Node(12)
+    root.right = Node(9)
+    root.right.left = Node(15)
+    root.right.right = Node(8)
+    print("The tree before the deletion:")
+    inorder(root)
+    key = 11
+    root = deletion(root, key)
+    print()
+    print("The tree after the deletion;")
+    inorder(root)
+    print()
