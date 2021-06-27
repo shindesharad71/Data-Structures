cacaf08676c785be959213c44b71ded26d1ba959
Sharad Shinde <shindesharad71@gmail.com>
2021-06-27 11:45:00 +0530

    Added: Binary tree using Array

---
+++ 05.BinaryTree/4_binary_tree_array_implementation.py
@@ -0,0 +1,7 @@
+# Binary Tree (Array implementation)
+# https://www.geeksforgeeks.org/binary-tree-array-implementation/
+
+
+# Python3 implementation of tree using array numbering starting from 0 to n-1.
+
+tree = [None] * 10
