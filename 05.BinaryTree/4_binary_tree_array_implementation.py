b1286078fecf91dc163032d4ba09ba5397ddb127
Sharad Shinde <shindesharad71@gmail.com>
2021-06-27 11:49:46 +0530

    Added: Binary tree using Array

--- 05.BinaryTree/4_binary_tree_array_implementation.py
+++ 05.BinaryTree/4_binary_tree_array_implementation.py
@@ -5,3 +5,24 @@
 # Python3 implementation of tree using array numbering starting from 0 to n-1.
 
 tree = [None] * 10
+
+
+def root(key):
+    if tree[0] is not None:
+        print("Tree already had root")
+    else:
+        tree[0] = key
+
+
+def set_left(key, parent):
+    if tree[parent] is None:
+        print("Cant set child at", (parent * 2) + 1, ", no parent found")
+    else:
+        tree[(parent * 2) + 1] = key
+
+
+def set_right(key, parent):
+    if tree[parent] == None:
+        print("Can't set child at", (parent * 2) + 2, ", no parent found")
+    else:
+        tree[(parent * 2) + 2] = key
