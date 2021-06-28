0c0fb33006556cbbaccfb8769ada37dc9db96890
Sharad Shinde <shindesharad71@gmail.com>
2021-06-28 10:10:42 +0530

    Added: Expression Tree

--- 05.BinaryTree/7_expression_tree.py
+++ 05.BinaryTree/7_expression_tree.py
@@ -1,25 +1,56 @@
 # Expression Tree
 # https://www.geeksforgeeks.org/expression-tree/
 
-
+# Create a tree node
 class Node:
     def __init__(self, data):
         self.data = data
         self.left = self.right = None
 
 
+# A utility function to check if 'char' is an operator
 def is_operator(char: str) -> bool:
     if char == "+" or char == "-" or char == "*" or char == "/" or char == "^":
         return True
     return False
 
 
+# A utility function to do inorder traversal
 def inorder(t: Node):
     if t is not None:
         inorder(t.left)
-        print(t.data)
+        print(t.data, end=" ")
         inorder(t.right)
 
 
-def construct_tree(postfix) -> str:
+# Returns root of constructed tree for given postfix expression
+def construct_tree(postfix: str) -> str:
     stack = []
+
+    # Traverse through every character of input expression
+    for char in postfix:
+        # if operand, simply push into stack
+        t = Node(char)
+        if not is_operator(char):
+            stack.append(t)
+        # Operator
+        else:
+            # Pop two top nodes
+            t1 = stack.pop()
+            t2 = stack.pop()
+
+            t.right = t1
+            t.left = t2
+
+            stack.append(t)
+
+    t = stack.pop()
+    return t
+
+
+# Driver Program
+if __name__ == "__main__":
+    postfix = "ab+ef*g*-"
+    r = construct_tree(postfix)
+    print("Infix expression is")
+    inorder(r)
