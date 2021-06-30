447e66395e9d56f54f2662c4a4025f364a39e561
Sharad Shinde <shindesharad71@gmail.com>
2021-06-30 10:24:12 +0530

    Added: Replace Node with Sum

--- 05.BinaryTree/02.Traversals/5_replace_node_with_sum.py
+++ 05.BinaryTree/02.Traversals/5_replace_node_with_sum.py
@@ -12,9 +12,9 @@ def store_inorder_traversal(root: Node, arr: list):
     if not root:
         return
 
-    store_inorder_traversal(root.left)
+    store_inorder_traversal(root.left, arr)
     arr.append(root.data)
-    store_inorder_traversal(root.right)
+    store_inorder_traversal(root.right, arr)
 
 
 def replace_node_with_sum(root: Node, arr: list, i: int):
@@ -24,3 +24,54 @@ def replace_node_with_sum(root: Node, arr: list, i: int):
     replace_node_with_sum(root.left, arr, i)
 
     root.data = arr[i[0] - 1] + arr[i[0] + 1]
+
+    i[0] += 1
+
+    replace_node_with_sum(root.right, arr, i)
+
+
+def replace_node_with_sum_util(root: Node):
+    if not root:
+        return
+
+    arr = []
+
+    arr.append(0)
+
+    store_inorder_traversal(root, arr)
+
+    arr.append(0)
+
+    i = [1]
+
+    replace_node_with_sum(root, arr, i)
+
+
+def preorder(root: Node):
+    if not root:
+        return
+
+    print(root.data, end=" ")
+
+    preorder(root.left)
+    preorder(root.right)
+
+
+# Driver Code
+if __name__ == "__main__":
+    # binary tree formation
+    root = Node(1)  # 1
+    root.left = Node(2)  # / \
+    root.right = Node(3)  # 2     3
+    root.left.left = Node(4)  # / \ / \
+    root.left.right = Node(5)  # 4 5 6 7
+    root.right.left = Node(6)
+    root.right.right = Node(7)
+
+    print("Preorder Traversal before tree modification:")
+    preorder(root)
+
+    replace_node_with_sum_util(root)
+    print()
+    print("Preorder Traversal after tree modification:")
+    preorder(root)
