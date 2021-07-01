70adbaef485d563b0896e9dfd8eeac43967040b0
Sharad Shinde <shindesharad71@gmail.com>
2021-07-01 18:18:16 +0530

    Added: Level Order Traversal

--- 05.BinaryTree/02.Traversals/10_level_order_traversal.py
+++ 05.BinaryTree/02.Traversals/10_level_order_traversal.py
@@ -1,7 +1,7 @@
 # Level Order Binary Tree Traversal
 # https://www.geeksforgeeks.org/level-order-tree-traversal/
 
-
+# A node structure
 class Node:
     def __init__(self, data):
         self.data = data
@@ -9,18 +9,25 @@ class Node:
 
 
 def height(node: Node) -> int:
+    """Compute the height of a tree--the number of nodes
+    along the longest path from the root node down to
+    the farthest leaf node
+    """
     if node is None:
         return 0
 
+    # Compute the height of each subtree
     lheight = height(node.left)
     rheight = height(node.right)
 
+    # Use the larger one
     if lheight > rheight:
         return lheight + 1
     else:
         return rheight + 1
 
 
+# Print nodes at a current level
 def print_current_level(root: Node, level: int):
     if root is None:
         return
@@ -32,6 +39,7 @@ def print_current_level(root: Node, level: int):
         print_current_level(root.right, level - 1)
 
 
+# Function to  print level order traversal of tree
 def print_level_order(root: Node):
     h = height(root)
 
