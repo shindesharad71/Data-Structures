6d6f6954d330ed2f8edb8166e35f2e6338749a67
Sharad Shinde <shindesharad71@gmail.com>
2021-07-01 17:04:46 +0530

    Added: Level Order Traversal

--- 05.BinaryTree/02.Traversals/10_level_order_traversal.py
+++ 05.BinaryTree/02.Traversals/10_level_order_traversal.py
@@ -1,2 +1,8 @@
 # Level Order Binary Tree Traversal
 # https://www.geeksforgeeks.org/level-order-tree-traversal/
+
+
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = self.right = None
