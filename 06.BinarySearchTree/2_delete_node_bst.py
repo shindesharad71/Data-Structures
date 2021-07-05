6d6ea7076cfa9519ebe50c9049e828facb115e66
Sharad Shinde <shindesharad71@gmail.com>
2021-07-05 19:28:27 +0530

    Added: Delete Node in BST

--- 06.BinarySearchTree/2_delete_node_bst.py
+++ 06.BinarySearchTree/2_delete_node_bst.py
@@ -3,88 +3,103 @@
 
 
 class Node:
-    def __init__(self, data):
-        self.data = data
+
+    # Constructor to create a new node
+    def __init__(self, key):
+        self.key = key
         self.left = self.right = None
 
 
+# A utility function to do
+# inorder traversal of BST
 def inorder(root: Node):
     if root:
         inorder(root.left)
-        print(root.data, end=" ")
+        print(root.key, end=" ")
         inorder(root.right)
 
 
-def insert(root: Node, data) -> Node:
-    if root is None:
-        return Node(data)
+# A utility function to insert a
+# new node with given key in BST
+def insert(node: Node, key: int) -> Node:
 
-    if root.data < data:
-        root.left = insert(root.left, data)
+    # If the tree is empty,
+    # return a new node
+    if node is None:
+        return Node(key)
+
+    # Otherwise recur down the tree
+    if key < node.key:
+        node.left = insert(node.left, key)
     else:
-        root.right = insert(root.right, data)
+        node.right = insert(node.right, key)
 
-    return root
-
-
-# Given a non-empty binary
-# search tree, return the node
-# with minimum key value
-# found in that tree. Note that the
-# entire tree does not need to be searched
-def min_value_node(root: Node):
-    current = root
-
-    # loop down to find the leftmost leaf
-    while current.left is not None:
-        current = current.left
-
-    return current
+    # return the (unchanged) node pointer
+    return node
 
 
-# Given a binary search tree and a key, this function
+# Given a binary search tree
+# and a key, this function
 # delete the key and returns the new root
-def delete_node(root: Node, key):
+def delete_node(root: Node, key: int) -> Node:
+    # Base Case
     if root is None:
         return root
 
-    # If the key to be deleted
-    # is smaller than the root's
-    # key then it lies in  left subtree
-    if key < root.data:
+    # Recursive calls for ancestors of
+    # node to be deleted
+    if key < root.key:
         root.left = delete_node(root.left, key)
+        return root
 
-    # If the kye to be delete
-    # is greater than the root's key
-    # then it lies in right subtree
-    elif key > root.data:
+    elif key > root.key:
         root.right = delete_node(root.right, key)
+        return root
 
-    # If key is same as root's key, then this is the node
-    # to be deleted
+    # We reach here when root is the node
+    # to be deleted.
+
+    # If root node is a leaf node
+
+    if root.left is None and root.right is None:
+        return None
+
+    # If one of the children is empty
+
+    if root.left is None:
+        temp = root.right
+        return temp
+
+    elif root.right is None:
+        temp = root.left
+        return temp
+
+    # If both children exist
+
+    succParent = root
+
+    # Find Successor
+
+    succ = root.right
+
+    while succ.left != None:
+        succParent = succ
+        succ = succ.left
+
+    # Delete successor.Since successor
+    # is always left child of its parent
+    # we can safely make successor's right
+    # right child as left of its parent.
+    # If there is no succ, then assign
+    # succ->right to succParent->right
+    if succParent != root:
+        succParent.left = succ.right
     else:
-        # Node with only one child or no child
-        if root.left is None:
-            temp = root.right
-            root = None
-            return temp
+        succParent.right = succ.right
 
-        elif root.right is None:
-            temp = root.left
-            root = None
-            return temp
+    # Copy Successor Data to root
 
-        # Node with two children:
-        # Get the inorder successor
-        # (smallest in the right subtree)
-        temp = min_value_node(root.right)
-
-        # Copy the inorder successor's
-        # content to this node
-        root.data = temp.data
-
-        # Delete the inorder successor
-        root.right = delete_node(root.right, temp.data)
+    root.key = succ.key
 
     return root
 
@@ -92,11 +107,11 @@ def delete_node(root: Node, key):
 # Driver code
 if __name__ == "__main__":
     """ Let us create following BST
-                  50
-               /     \
-              30      70
-             /  \    /  \
-           20   40  60   80 """
+                50
+            /	 \
+            30	 70
+            / \ / \
+        20 40 60 80 """
 
     root = None
     root = insert(root, 50)
@@ -112,18 +127,15 @@ if __name__ == "__main__":
 
     print("\nDelete 20")
     root = delete_node(root, 20)
-
     print("Inorder traversal of the modified tree")
     inorder(root)
 
     print("\nDelete 30")
     root = delete_node(root, 30)
-
     print("Inorder traversal of the modified tree")
     inorder(root)
 
     print("\nDelete 50")
     root = delete_node(root, 50)
-
     print("Inorder traversal of the modified tree")
     inorder(root)
