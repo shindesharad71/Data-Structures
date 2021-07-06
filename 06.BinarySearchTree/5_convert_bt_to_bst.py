feb1e9f2e6131f7ef9b8e4666877179f917949a6
Sharad Shinde <shindesharad71@gmail.com>
2021-07-06 19:11:00 +0530

    Added: Convert BT to BST

---
+++ 06.BinarySearchTree/5_convert_bt_to_bst.py
@@ -0,0 +1,8 @@
+# Binary Tree to Binary Search Tree Conversion
+# https://www.geeksforgeeks.org/binary-tree-to-binary-search-tree-conversion/
+
+# Binary Tree Node
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.left = self.right = None
