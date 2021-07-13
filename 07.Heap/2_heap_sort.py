e80fbfeef0ec237c34682dc96010c1ab863e5019
Sharad Shinde <shindesharad71@gmail.com>
2021-07-13 16:55:22 +0530

    Added: Heap Sort

--- 07.Heap/2_heap_sort.py
+++ 07.Heap/2_heap_sort.py
@@ -1,2 +1,48 @@
 # Heap Sort
 # https://www.geeksforgeeks.org/heap-sort/
+
+
+def heapify(arr: list, n: int, i: int):
+    largest = i  # Initialize largest as root
+    l = 2 * i + 1  # left = 2*i + 1
+    r = 2 * i + 2  # right = 2*i + 2
+
+    # See if left child of root exists and is
+    # greater than root
+    if l < n and arr[largest] < arr[l]:
+        largest = l
+
+    # See if right child of root exists and is
+    # greater than root
+    if r < n and arr[largest] < arr[r]:
+        largest = r
+
+    # Change root, if needed
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]  # swap
+
+        # Heapify the root.
+        heapify(arr, n, largest)
+
+
+def heap_sort(arr):
+    n = len(arr)
+
+    # Build a max heap.
+    for i in range(n // 2 - 1, -1, -1):
+        heapify(arr, n, i)
+
+    # One by one extract elements
+    for i in range(n - 1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]  # swap
+        heapify(arr, i, 0)
+
+
+# Driver code
+if __name__ == "__main__":
+    arr = [12, 11, 13, 5, 6, 7]
+    heap_sort(arr)
+    n = len(arr)
+    print("Sorted array is")
+    for i in range(n):
+        print(arr[i], end=" ")
