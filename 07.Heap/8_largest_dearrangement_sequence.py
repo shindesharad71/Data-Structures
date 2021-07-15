68ac5c0c7f7b3e2f98b40b615cce1686d0bba296
Sharad Shinde <shindesharad71@gmail.com>
2021-07-15 09:57:24 +0530

    Added: Largest Dearrangement Sequence

--- 07.Heap/8_largest_dearrangement_sequence.py
+++ 07.Heap/8_largest_dearrangement_sequence.py
@@ -10,3 +10,36 @@ def print_largest(seq, N):
 
     for i in range(N):
         pq.append(seq[i])
+
+    # Fill Up res[] from left to right
+    for i in range(N):
+        pq.sort(reverse=True)
+        d = pq[0]
+        del pq[0]
+
+        if d != seq[i] or i == N - 1:
+            res[i] = d
+        else:
+            # New Element popped equals the element
+            # in original sequence. Get the next
+            # largest element
+            res[i] = pq[0]
+            del pq[0]
+            pq.append(d)
+
+        # If given sequence is in descending order then
+        # we need to swap last two elements again
+        if res[N - 1] == seq[N - 1]:
+            res[N - 1] = res[N - 2]
+            res[N - 2] = seq[N - 1]
+
+    print("Largest Derangement")
+    for i in range(N):
+        print(res[i], end=" ")
+
+
+# Driver code
+if __name__ == "__main__":
+    seq = [92, 3, 52, 13, 2, 31, 1]
+    n = len(seq)
+    print_largest(seq, n)
