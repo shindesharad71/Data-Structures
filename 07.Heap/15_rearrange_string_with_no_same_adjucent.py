e6dafdd355d226aad66c5ad03bbb088a2be8f049
Sharad Shinde <shindesharad71@gmail.com>
2021-07-17 11:10:10 +0530

    Added: Rearrange string with no duplicates

---
+++ 07.Heap/15_rearrange_string_with_no_same_adjucent.py
@@ -0,0 +1,11 @@
+# Rearrange characters in a string such that no two adjacent are same
+# https://www.geeksforgeeks.org/rearrange-characters-string-no-two-adjacent/
+
+
+def rearrange_string(string: str):
+    pass
+
+
+if __name__ == "__main__":
+    s = "bbbaa"
+    print(rearrange_string(s))
