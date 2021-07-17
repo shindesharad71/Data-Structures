23e597e286f4471c808838c7c3f993ac90b23cb9
Sharad Shinde <shindesharad71@gmail.com>
2021-07-17 11:43:30 +0530

    Added: Rearrange String With No Adjecent Duplicates

--- 07.Heap/15_rearrange_string_with_no_same_adjucent.py
+++ 07.Heap/15_rearrange_string_with_no_same_adjucent.py
@@ -3,9 +3,27 @@
 
 
 def rearrange_string(string: str):
-    pass
+    n = len(string)
+    res = ""
+    prev = ""
+
+    map = {}
+
+    for i in range(n):
+        if string[i] in map:
+            map[string[i]] += 1
+        else:
+            map[string[i]] = 1
+
+    while len(res) < n:
+        for val, key in map.items():
+            if prev != val and key > 0:
+                res = f"{res}{val}"
+                map[val] -= 1
+
+    return res
 
 
 if __name__ == "__main__":
-    s = "bbbaa"
+    s = "aaabc"
     print(rearrange_string(s))
