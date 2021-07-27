b5226575100e7fceb1bc5b6cc3e764be81acf6d9
Sharad Shinde <shindesharad71@gmail.com>
2021-07-27 09:49:37 +0530

    Added: Find Pair

---
+++ 08.Hashing/19_find_pairs_in_array.py
@@ -0,0 +1,13 @@
+# Find pairs in array whose sums already exist in array
+# https://www.geeksforgeeks.org/find-pairs-in-array-whose-sums-already-exist-in-array/
+
+
+def find_pair(arr: list, n: int):
+    pass
+
+
+# Driver code
+if __name__ == "__main__":
+    arr = [10, 4, 8, 13, 5]
+    n = len(arr)
+    find_pair(arr, n)
