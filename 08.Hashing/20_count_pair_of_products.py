74ad8b3766454f16585bed9b5048ab321f15689b
Sharad Shinde <shindesharad71@gmail.com>
2021-07-28 09:14:21 +0530

    Added: Count Pair of Products

--- 08.Hashing/20_count_pair_of_products.py
+++ 08.Hashing/20_count_pair_of_products.py
@@ -3,4 +3,22 @@
 
 
 def count_pairs(arr: list, n: int):
-    pass
+    result = 0
+
+    for i in range(n):
+        for j in range(i + 1, n):
+            product = arr[i] * arr[j]
+
+            for k in range(n):
+                if arr[k] == product:
+                    result += 1
+                    break
+
+    return result
+
+
+# Driver program
+if __name__ == "__main__":
+    arr = [6, 2, 4, 12, 5, 3]
+    n = len(arr)
+    print(count_pairs(arr, n))
