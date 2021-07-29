d2ce053370d92a74a6b9a3f6852950d1062abc44
Sharad Shinde <shindesharad71@gmail.com>
2021-07-29 19:09:31 +0530

    Added: Most Frequent

---
+++ 08.Hashing/21_minimum_number_of_elements_no_common.py
@@ -0,0 +1,14 @@
+# Find k numbers with most occurrences in the given array
+# https://www.geeksforgeeks.org/find-k-numbers-occurrences-given-array/
+
+
+def most_frequent(arr: list, n: int, k):
+    pass
+
+
+# Driver code
+if __name__ == "__main__":
+    arr = [3, 1, 4, 4, 5, 2, 6, 1]
+    n = 8
+    k = 2
+    most_frequent(arr, n, k)
