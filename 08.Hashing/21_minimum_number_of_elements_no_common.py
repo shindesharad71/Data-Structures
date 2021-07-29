804a1b830d91e1e0672814f024536ddffe16c023
Sharad Shinde <shindesharad71@gmail.com>
2021-07-29 19:10:38 +0530

    Added: Most Frequent

--- 08.Hashing/21_minimum_number_of_elements_no_common.py
+++ 08.Hashing/21_minimum_number_of_elements_no_common.py
@@ -3,7 +3,24 @@
 
 
 def most_frequent(arr: list, n: int, k):
-    pass
+    um = {}
+    for i in range(n):
+        if arr[i] in um:
+            um[arr[i]] += 1
+        else:
+            um[arr[i]] = 1
+    a = [0] * (len(um))
+    j = 0
+    for i in um:
+        a[j] = [i, um[i]]
+        j += 1
+    a = sorted(a, key=lambda x: x[0], reverse=True)
+    a = sorted(a, key=lambda x: x[1], reverse=True)
+
+    # display the top k numbers
+    print(k, "numbers with most occurrences are:")
+    for i in range(k):
+        print(a[i][0], end=" ")
 
 
 # Driver code
