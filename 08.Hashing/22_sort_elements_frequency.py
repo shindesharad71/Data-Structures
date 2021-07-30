72baa7413d19b9a035928b5d31608449309fe962
Sharad Shinde <shindesharad71@gmail.com>
2021-07-30 09:35:04 +0530

    Added: Sort Elements by Frequency

---
+++ 08.Hashing/22_sort_elements_frequency.py
@@ -0,0 +1,13 @@
+# Sort elements by frequency | Set 4 (Efficient approach using hash)
+# https://www.geeksforgeeks.org/sort-elements-frequency-set-4-efficient-approach-using-hash/
+
+from collections import defaultdict
+
+
+def sort_by_freq(arr: list, n: int):
+    pass
+
+
+if __name__ == "__main__":
+    arr = [2, 5, 2, 6, -1, 9999999, 5, 8, 8, 8]
+    d = defaultdict()
