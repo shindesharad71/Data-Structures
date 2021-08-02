a378a57d292457e8b734b57d6fc274c61d7f2592
Sharad Shinde <shindesharad71@gmail.com>
2021-08-02 09:59:38 +0530

    Added: Add Two Numbers

--- 00.LeetCode/2_add_two_numbers.py
+++ 00.LeetCode/2_add_two_numbers.py
@@ -10,7 +10,23 @@ class ListNode:
 
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        pass
+        first_num = self.list_to_number(l1)
+        second_num = self.list_to_number(l2)
+
+        sum = first_num + second_num
+
+    @staticmethod
+    def insert(node: ListNode, val: int) -> ListNode:
+        new_node = ListNode(val)
+        if node is None:
+            return new_node
+
+        current = node
+        while current.next:
+            current = current.next
+
+        current.next = new_node
+        return node
 
     @staticmethod
     def list_to_number(node: ListNode) -> int:
@@ -24,7 +40,33 @@ class Solution:
 
         return int(num)
 
+    @staticmethod
+    def print_list(node: ListNode):
+        if node is None:
+            return
+
+        current = node
+        while current:
+            print(current.val, end=" ")
+            current = current.next
+        print()
+
 
 # Driver Code
 if __name__ == "__main__":
     s = Solution()
+
+    arr1 = [2, 4, 3]
+    arr2 = [5, 6, 4]
+
+    l1 = ListNode(arr1[0])
+    l2 = ListNode(arr2[0])
+
+    for i in range(1, len(arr1)):
+        l1 = s.insert(l1, arr1[i])
+
+    for i in range(1, len(arr2)):
+        l2 = s.insert(l2, arr2[i])
+
+    s.print_list(l1)
+    s.print_list(l2)
