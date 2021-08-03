9c07b835a8cd386ea0bdc8f3b3177f790ac554c5
Sharad Shinde <shindesharad71@gmail.com>
2021-08-03 10:48:40 +0530

    Updated: Longest Non-repeating Substring

--- 00.LeetCode/3_longest_substring.py
+++ 00.LeetCode/3_longest_substring.py
@@ -4,11 +4,24 @@
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        pass
+        collection = []
+        longest = 0
+        for ch in s:
+
+            if ch in collection:
+                longest = max(longest, len(collection))
+                index = collection.index(ch)
+                collection = collection[
+                    index + 1 :
+                ]  # remove all items before the repeated item including itself
+
+            collection.append(ch)
+
+        return max(longest, len(collection))
 
 
 # Driver Code
 if __name__ == "__main__":
     sln = Solution()
-    s = "abcabcbb"
+    s = "dvdf"
     print(sln.lengthOfLongestSubstring(s))
