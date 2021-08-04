aa400bf87b51724989b01d7a079d75b0d2aebd43
Sharad Shinde <shindesharad71@gmail.com>
2021-08-04 09:42:47 +0530

    Added: Median of Sorted Array

---
+++ 00.LeetCode/4_median_sorted_array.py
@@ -0,0 +1,17 @@
+# 4. Median of Two Sorted Arrays
+# https://leetcode.com/problems/median-of-two-sorted-arrays/
+
+from typing import List
+
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        pass
+
+
+# Driver Code
+if __name__ == "__main__":
+    s = Solution()
+    nums1 = []
+    nums2 = []
+    print(s.findMedianSortedArrays(nums1, nums2))
