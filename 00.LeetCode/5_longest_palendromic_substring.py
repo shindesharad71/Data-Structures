a00009db7a322b36e0df6759ffce4c85ab54335e
Sharad Shinde <shindesharad71@gmail.com>
2021-08-05 09:56:20 +0530

    Added: Longest Palendromic Substring

---
+++ 00.LeetCode/5_longest_palendromic_substring.py
@@ -0,0 +1,14 @@
+# 5. Longest Palindromic Substring
+# https://leetcode.com/problems/longest-palindromic-substring/
+
+
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        pass
+
+
+# Driver Code
+if __name__ == "__main__":
+    sln = Solution()
+    s = "babad"
+    print(sln.longestPalindrome(s))
