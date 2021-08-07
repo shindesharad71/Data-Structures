24c4992095cd5a93b70f7e6be57d542567deb232
Sharad Shinde <shindesharad71@gmail.com>
2021-08-07 21:06:37 +0530

    Added: Reverse Integer

---
+++ 00.LeetCode/7_reverse_integer.py
@@ -0,0 +1,14 @@
+# 7. Reverse Integer
+# https://leetcode.com/problems/reverse-integer/
+
+
+class Solution:
+    def reverse(self, x: int) -> int:
+        pass
+
+
+# Driver Code
+if __name__ == "__main__":
+    sln = Solution()
+    x = 123
+    print(sln.reverse(x))
