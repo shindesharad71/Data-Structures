d7e273800ad18ce2f181d0b8d2f8e8825d661c93
Sharad Shinde <shindesharad71@gmail.com>
2021-08-08 10:52:28 +0530

    Added: String to Int

--- 00.LeetCode/8_string_to_int.py
+++ 00.LeetCode/8_string_to_int.py
@@ -4,24 +4,24 @@
 
 class Solution:
     def myAtoi(self, s: str) -> int:
-        if len(s) > 0 and (s[0].isdigit() or s[0] == "-" or s[0] == "+" or s[0] == " "):
+        s = s.strip()
+        if len(s) > 0 and (s[0].isdigit() or s[0] == "-" or s[0] == "+"):
             op = ""
-            LOWER_BOUND = -2147483648
-            UPPER_BOUND = 2147483647
+            s = list(s.strip())
+            sign = -1 if s[0] == "-" else 1
 
-            for c in s:
-                if c.isdigit() or c == "-" or c == "+" or c == " ":
-                    if c != " ":
-                        op = f"{op}{c}"
+            if s[0] in ("+", "-"):
+                del s[0]
+
+            for i in range(len(s)):
+                if s[i].isdigit():
+                    op = f"{op}{s[i]}"
                 else:
                     break
 
             if len(op) > 0:
                 op = int(op)
-                if op.bit_length() < 32:
-                    return op
-                else:
-                    return LOWER_BOUND if op < 0 else UPPER_BOUND
+                return max(-(2 ** 31), min(sign * op, 2 ** 31 - 1))
 
         return 0
 
@@ -29,7 +29,7 @@ class Solution:
 # Driver code
 if __name__ == "__main__":
     # s = "-91283472332"
-    # s = "    -+56"
+    s = "    -56"
 
     sln = Solution()
     print(sln.myAtoi(s))
