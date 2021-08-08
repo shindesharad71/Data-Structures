911270fda505c3245263246eeb81167b3f135e1b
Sharad Shinde <shindesharad71@gmail.com>
2021-08-08 11:19:28 +0530

    Updated: Palidrome Number

--- 00.LeetCode/9_palindrome_number.py
+++ 00.LeetCode/9_palindrome_number.py
@@ -4,11 +4,15 @@
 
 class Solution:
     def isPalindrome(self, x: int) -> bool:
+        num_str = str(x)
+        if num_str[0] in ("-", "+"):
+            return False
+
         return x == int(str(x)[::-1])
 
 
 # Driver Code
 if __name__ == "__main__":
     sln = Solution()
-    x = 121
+    x = -101
     print(sln.isPalindrome(x))
