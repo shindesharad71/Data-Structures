be67fb452090dcbe4d5325701e4b82abeeee6210
Sharad Shinde <shindesharad71@gmail.com>
2021-08-08 10:34:55 +0530

    Updated: String to Int

--- 00.LeetCode/8_string_to_int.py
+++ 00.LeetCode/8_string_to_int.py
@@ -5,14 +5,31 @@
 class Solution:
     def myAtoi(self, s: str) -> int:
         if len(s) > 0 and (s[0].isdigit() or s[0] == "-" or s[0] == "+" or s[0] == " "):
-            return s
+            op = ""
+            LOWER_BOUND = -2147483648
+            UPPER_BOUND = 2147483647
+
+            for c in s:
+                if c.isdigit() or c == "-" or c == "+" or c == " ":
+                    if c != " ":
+                        op = f"{op}{c}"
+                else:
+                    break
+
+            if len(op) > 0:
+                op = int(op)
+                if op.bit_length() < 32:
+                    return op
+                else:
+                    return LOWER_BOUND if op < 0 else UPPER_BOUND
 
         return 0
 
 
 # Driver code
 if __name__ == "__main__":
-    # s = "words and 987"
-    s = "   987"
+    # s = "-91283472332"
+    # s = "    -+56"
+
     sln = Solution()
     print(sln.myAtoi(s))
