527cb51916097b656064f6e4bc8c9ddc54f14022
Sharad Shinde <shindesharad71@gmail.com>
2021-08-08 10:09:55 +0530

    Added: String to Int

---
+++ 00.LeetCode/8_string_to_int.py
@@ -0,0 +1,14 @@
+# 8. String to Integer (atoi)
+# https://leetcode.com/problems/string-to-integer-atoi/
+
+
+class Solution:
+    def myAtoi(self, s: str) -> int:
+        pass
+
+
+# Driver code
+if __name__ == "__main__":
+    s = "words and 987"
+    sln = Solution()
+    print(sln.myAtoi())
