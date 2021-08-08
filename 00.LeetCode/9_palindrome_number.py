98ed5f2361ba6956c0d5cbbbfeca8563e592d494
Sharad Shinde <shindesharad71@gmail.com>
2021-08-08 11:12:43 +0530

    Added: Is Number Palindrome

---
+++ 00.LeetCode/9_palindrome_number.py
@@ -0,0 +1,14 @@
+# 9. Palindrome Number
+# https://leetcode.com/problems/palindrome-number/
+
+
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        pass
+
+
+# Driver Code
+if __name__ == "__main__":
+    sln = Solution()
+    x = 123
+    print(sln.isPalindrome(x))
