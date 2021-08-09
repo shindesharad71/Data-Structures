033f25f186fa2b25b4753e46d74ffe66d86f6000
Sharad Shinde <shindesharad71@gmail.com>
2021-08-09 20:02:34 +0530

    Added: Roman to Integer

--- 00.LeetCode/13_roman_integer.py
+++ 00.LeetCode/13_roman_integer.py
@@ -4,11 +4,24 @@
 
 class Solution:
     def romanToInt(self, s: str) -> int:
-        pass
+        MAPPING = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+
+        number, prev = 0, 0
+        for char in s[::-1]:
+            if MAPPING[char] >= prev:
+                number += MAPPING[char]  # sum the value iff previous value same or more
+            else:
+                number -= MAPPING[
+                    char
+                ]  # subtract when value is like "IV" --> 5-1, "IX" --> 10 -1 etc
+
+            prev = MAPPING[char]
+
+        return number
 
 
 # Driver Code
 if __name__ == "__main__":
     sln = Solution()
-    s = "III"
+    s = "IX"
     print(sln.romanToInt(s))
