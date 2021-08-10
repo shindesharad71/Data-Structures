f936ac58c69b2ee131c85ea1193aabdd0dee1a5a
Sharad Shinde <shindesharad71@gmail.com>
2021-08-10 19:15:07 +0530

    Added: Valid Parenthesis

---
+++ 00.LeetCode/20_valid_parenthesis.py
@@ -0,0 +1,14 @@
+# 20. Valid Parentheses
+# https://leetcode.com/problems/valid-parentheses/
+
+
+class Solution:
+    def isValid(self, s: str) -> bool:
+        pass
+
+
+# Driver Code
+if __name__ == "__main__":
+    sln = Solution()
+    s = "()[]{}"
+    print(sln.isValid(s))
