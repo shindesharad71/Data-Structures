d33b9fc65b38205b08086d07100f24a93537fcb4
Sharad Shinde <shindesharad71@gmail.com>
2021-08-10 18:53:47 +0530

    Added: Longest Common Prefix

--- 00.LeetCode/14_longest_common_prefix.py
+++ 00.LeetCode/14_longest_common_prefix.py
@@ -1,10 +1,17 @@
 # 14. Longest Common Prefix
 # https://leetcode.com/problems/longest-common-prefix/
+from typing import List
 
 
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
-        pass
+        prefix = ""
+        for i in range(len(strs[0])):
+            for j in range(1, len(strs)):
+                if strs[0][i] != strs[j][i]:
+                    break
+            prefix += strs[0][i]
+        return prefix
 
 
 # Driver Code
