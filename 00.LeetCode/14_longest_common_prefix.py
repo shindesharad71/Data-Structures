fe5c678e22176bd3f6d23f32b65d897bb861257d
Sharad Shinde <shindesharad71@gmail.com>
2021-08-10 18:42:08 +0530

    Added: Longest Common Prefix

---
+++ 00.LeetCode/14_longest_common_prefix.py
@@ -0,0 +1,14 @@
+# 14. Longest Common Prefix
+# https://leetcode.com/problems/longest-common-prefix/
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        pass
+
+
+# Driver Code
+if __name__ == "__main__":
+    sln = Solution()
+    strs = ["flower", "flow", "flight"]
+    print(sln.longestCommonPrefix(strs))
