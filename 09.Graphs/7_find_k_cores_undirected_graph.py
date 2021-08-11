1bd3b4426d4491be68dba6f22c0dbcc225bd3903
Sharad Shinde <shindesharad71@gmail.com>
2021-08-11 10:07:05 +0530

    Added: Find K Cores of Undirected Graph

--- 09.Graphs/7_find_k_cores_undirected_graph.py
+++ 09.Graphs/7_find_k_cores_undirected_graph.py
@@ -41,3 +41,27 @@ class Graph:
                     if degree[j] >= k:
                         print("-> " + str(j), end=" ")
                 print()
+
+
+# Driver Code
+if __name__ == "__main__":
+    k = 3
+    g1 = Graph()
+    g1.addEdge(0, 1)
+    g1.addEdge(0, 2)
+    g1.addEdge(1, 2)
+    g1.addEdge(1, 5)
+    g1.addEdge(2, 3)
+    g1.addEdge(2, 4)
+    g1.addEdge(2, 5)
+    g1.addEdge(2, 6)
+    g1.addEdge(3, 4)
+    g1.addEdge(3, 6)
+    g1.addEdge(3, 7)
+    g1.addEdge(4, 6)
+    g1.addEdge(4, 7)
+    g1.addEdge(5, 6)
+    g1.addEdge(5, 8)
+    g1.addEdge(6, 7)
+    g1.addEdge(6, 8)
+    g1.PrintKCores(k)
