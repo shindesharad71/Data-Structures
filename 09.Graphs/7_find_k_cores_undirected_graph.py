db8adba328da652f95fbd8548a607997d0a53159
Sharad Shinde <shindesharad71@gmail.com>
2021-08-11 10:05:54 +0530

    Added: Find K Cores of Undirected Graph

--- 09.Graphs/7_find_k_cores_undirected_graph.py
+++ 09.Graphs/7_find_k_cores_undirected_graph.py
@@ -7,3 +7,37 @@ from collections import defaultdict
 class Graph:
     def __init__(self):
         self.graph = defaultdict(list)
+
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+        self.graph[v].append(u)
+
+    def DFSUtil(self, v, visited, vDegree, k):
+        visited.add(v)
+
+        for i in self.graph[v]:
+            if vDegree[v] < k:
+                vDegree[i] = vDegree[i] - 1
+
+            if i not in visited:
+                self.DFSUtil(i, visited, vDegree, k)
+
+    def PrintKCores(self, k):
+        visit = set()
+        degree = defaultdict(lambda: 0)
+
+        for i in list(self.graph):
+            degree[i] = len(self.graph[i])
+
+        for i in list(self.graph):
+            if i not in visit:
+                self.DFSUtil(i, visit, degree, k)
+
+        for i in list(self.graph):
+            if degree[i] >= k:
+                print(str("\n [ ") + str(i) + str(" ]"), end=" ")
+
+                for j in self.graph[i]:
+                    if degree[j] >= k:
+                        print("-> " + str(j), end=" ")
+                print()
