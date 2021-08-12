ce2c576e3a2129772200da88b8523da3ec52cb08
Sharad Shinde <shindesharad71@gmail.com>
2021-08-12 09:32:39 +0530

    Added: Interative Depth First

--- 09.Graphs/8_iterative_depth_first_search.py
+++ 09.Graphs/8_iterative_depth_first_search.py
@@ -6,3 +6,23 @@ class Graph:
     def __init__(self, v):
         self.v = v
         self.adj = [[] for i in range(v)]
+
+    def adj(self, v, w):
+        self.adj[v].append(w)
+
+    def DFS(self, s):
+        visited = [False for i in range(self.v)]
+
+        stack = [s]
+
+        while len(stack):
+            s = stack[-1]
+            stack.pop()
+
+            if not visited[s]:
+                print(s, end=" ")
+                visited[s] = True
+
+            for node in self.adj[s]:
+                if not visited[node]:
+                    stack.append(node)
