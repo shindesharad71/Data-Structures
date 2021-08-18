02965a0850323943207e81988a43f04d349bea50
Sharad Shinde <shindesharad71@gmail.com>
2021-08-18 11:17:02 +0530

    Added: Length of last word

---
+++ 00.LeetCode/58_length_of_last_word.py
@@ -0,0 +1,14 @@
+# 58. Length of Last Word
+# https://leetcode.com/problems/length-of-last-word/
+
+
+class Solution:
+    def lengthOfLastWord(self, s: str) -> int:
+        pass
+
+
+# Driver Code
+if __name__ == "__main__":
+    sln = Solution()
+    s = "Hello World"
+    print(sln.lengthOfLastWord(s))
