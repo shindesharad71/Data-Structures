e9ced2270299b15e085d334758e754a91df3fe99
Sharad Shinde <shindesharad71@gmail.com>
2021-08-18 18:22:27 +0530

    Added: Plus One

--- 00.LeetCode/66_plus_one.py
+++ 00.LeetCode/66_plus_one.py
@@ -6,11 +6,12 @@ from typing import List
 
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
+        digits[-1] = digits[-1] + 1
         return digits
 
 
 # Driver Code
 if __name__ == "__main__":
     sln = Solution()
-    digits = [1, 2, 3]
+    digits = [0]
     print(sln.plusOne(digits))
