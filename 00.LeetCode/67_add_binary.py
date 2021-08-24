81270a46c5fed6d42eb3b41464d0661a7a8d3b44
Sharad Shinde <shindesharad71@gmail.com>
2021-08-24 10:08:53 +0530

    Added: Binary Addition

--- 00.LeetCode/67_add_binary.py
+++ 00.LeetCode/67_add_binary.py
@@ -5,3 +5,11 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
         return format(int(a, 2) + int(b, 2), "b")
+
+
+# Driver Code
+if __name__ == "__main__":
+    sln = Solution()
+    a = "1010"
+    b = "1011"
+    print(sln.addBinary(a, b))
