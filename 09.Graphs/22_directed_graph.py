a4d5e68b284aadf5e502ceacb774c4ca67d4f320
Sharad Shinde <shindesharad71@gmail.com>
2021-08-26 09:33:59 +0530

    Added: Directed Graph

--- 09.Graphs/22_directed_graph.py
+++ 09.Graphs/22_directed_graph.py
@@ -1,2 +1,64 @@
 # Detect Cycle in a Directed Graph
 # https://www.geeksforgeeks.org/detect-cycle-in-a-graph/
+
+
+# Python program to detect cycle
+# in a graph
+
+from collections import defaultdict
+
+
+class Graph:
+    def __init__(self, vertices):
+        self.graph = defaultdict(list)
+        self.V = vertices
+
+    def addEdge(self, u, v):
+        self.graph[u].append(v)
+
+    def isCyclicUtil(self, v, visited, recStack):
+
+        # Mark current node as visited and
+        # adds to recursion stack
+        visited[v] = True
+        recStack[v] = True
+
+        # Recur for all neighbours
+        # if any neighbour is visited and in
+        # recStack then graph is cyclic
+        for neighbour in self.graph[v]:
+            if not visited[neighbour]:
+                if self.isCyclicUtil(neighbour, visited, recStack):
+                    return True
+            elif recStack[neighbour]:
+                return True
+
+        # The node needs to be poped from
+        # recursion stack before function ends
+        recStack[v] = False
+        return False
+
+    # Returns true if graph is cyclic else false
+    def isCyclic(self):
+        visited = [False] * (self.V + 1)
+        recStack = [False] * (self.V + 1)
+        for node in range(self.V):
+            if not visited[node]:
+                if self.isCyclicUtil(node, visited, recStack):
+                    return True
+        return False
+
+
+# Driver Code
+if __name__ == "__main__":
+    g = Graph(4)
+    g.addEdge(0, 1)
+    g.addEdge(0, 2)
+    g.addEdge(1, 2)
+    g.addEdge(2, 0)
+    g.addEdge(2, 3)
+    g.addEdge(3, 3)
+    if g.isCyclic() == 1:
+        print("Graph has a cycle")
+    else:
+        print("Graph has no cycle")
