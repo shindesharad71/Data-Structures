a7cd247228a723e880bccd3aa24c072722785f6d
Sharad Shinde <shindesharad71@gmail.com>
2021-09-03 09:32:05 +0530

    Added: Smallest Binary

--- 09.Graphs/30_smallest_binary_digits.py
+++ 09.Graphs/30_smallest_binary_digits.py
@@ -1,2 +1,28 @@
 # Find the smallest binary digit multiple of given number
 # https://www.geeksforgeeks.org/find-the-smallest-binary-digit-multiple-of-given-number/
+
+
+def getMinimumMultipleOfBinaryDigit(A):
+
+    # queue for BFS
+    q = []
+
+    # set of visited remainders
+    visitedRem = set([])
+    t = "1"
+    q.append(t)
+    while q:
+        t = q.pop(0)
+        rem = int(t) % A
+        if rem == 0:
+            return t
+        if rem not in visitedRem:
+            visitedRem.add(rem)
+            q.append(t + "0")
+            q.append(t + "1")
+
+
+# Driver code
+if __name__ == "__main__":
+    n = 12
+    print(getMinimumMultipleOfBinaryDigit(n))
